c8f60d6964bf924b7d90dbc2e53107afcc2b3567
wangguofu@ym.com <wangguofu@ym.com>
2021-01-20 11:30:55 +0800

    de

--- src/main/java/com/tools/st/controller/TableInfoController.java
+++ src/main/java/com/tools/st/controller/TableInfoController.java
@@ -2,9 +2,6 @@ package com.tools.st.controller;
 
 import java.util.List;
 
-import com.tools.st.entity.SysComPageConfig;
-import com.tools.st.entity.SysComTableConfig;
-import com.tools.st.service.PageConfigService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,9 +19,6 @@ public class TableInfoController {
   @Autowired
   TableInfoService tableInfoService;
 
-  @Autowired
-  PageConfigService pageConfigService;
-
   @ApiOperation("字段信息")
   @GetMapping("database/{schema}/{tabName}")
   public List<ColumnInfo> getColumns(@PathVariable String schema, @PathVariable String tabName) {
@@ -36,20 +30,5 @@ public class TableInfoController {
     return tableInfoService.getAllTables();
   }
 
-//  @ApiOperation("页面配置")
-//  @GetMapping("/config/page/{pageId}")
-//  public SysComPageConfig getPage(@PathVariable String pageId) {
-//    return pageConfigService.getPage(pageId);
-//  }
 
-  @ApiOperation("所有页面配置")
-  @GetMapping("/config/pages")
-  public List<SysComPageConfig> getPageConfigList() {
-    return pageConfigService.getPages();
-  }
-
-  @GetMapping("/tableConfig/{tableId}")
-  public List<SysComTableConfig> getTableConfig(@PathVariable String tableId) {
-    return pageConfigService.getTableConfig(tableId);
-  }
 }
