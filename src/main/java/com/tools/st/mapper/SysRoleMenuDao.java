67c384b169d8365aef94f021c329cdf3d27b95e5
wangguofu@ym.com <wangguofu@ym.com>
2021-01-20 11:25:09 +0800

    de

--- src/main/java/com/tools/st/mapper/SysRoleMenuDao.java
+++
@@ -1,7 +0,0 @@
-package com.tools.st.mapper;
-
-import com.tools.st.mapper.base.GenericMapper;
-import com.tools.st.entity.SysRoleMenu;
-
-public interface SysRoleMenuDao extends GenericMapper<SysRoleMenu, Long> {
-}
